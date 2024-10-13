5f09bd7b48ac4a2da562667963b971b1c4e70b22
SrGarZito <141846948+SrGarZito@users.noreply.github.com>
2024-10-13 01:18:08 -0300

    Actualización Interfaz (All Data)

--- namek.js
+++ namek.js
@@ -1,33 +1,87 @@
 
+//Creación del mapa de sagas de Namek//
+
 function namek(){
     let SVG = document.getElementById("namek");
-    SVG.setAttribute("width", "400");
-    SVG.setAttribute("height", "687");
+    SVG.setAttribute("width", "645");
+    SVG.setAttribute("height", "1108");
 }
+
+//Creación de los fondos rectangulares//
 
 function fondo1(){
     let SVG = document.getElementById("fondo1");
-    SVG.setAttribute("width", "1200");
-    SVG.setAttribute("height", "800");
+    SVG.setAttribute("width", "1000");
+    SVG.setAttribute("height", "1220");
 }
 
 function fondo2(){
     let SVG = document.getElementById("fondo2");
-    SVG.setAttribute("width", "300");
-    SVG.setAttribute("height", "450");
-}
-
-function fondo3(){
-    let SVG = document.getElementById("fondo3");
     SVG.setAttribute("width", "500");
     SVG.setAttribute("height", "800");
 }
 
-function fondo4(){
-    let SVG = document.getElementById("fondo4");
-    SVG.setAttribute("width", "500");
-    SVG.setAttribute("height", "800");
+function enlace1(){
+    let SVG = document.getElementById("enlace1");
+    SVG.setAttribute("width", "700");
+    SVG.setAttribute("height", "180");
 }
+
+function enlace2(){
+    let SVG = document.getElementById("enlace2");
+    SVG.setAttribute("width", "700");
+    SVG.setAttribute("height", "180");
+}
+
+function enlace3(){
+    let SVG = document.getElementById("enlace3");
+    SVG.setAttribute("width", "700");
+    SVG.setAttribute("height", "180");
+}
+
+function enlace4(){
+    let SVG = document.getElementById("enlace4");
+    SVG.setAttribute("width", "700");
+    SVG.setAttribute("height", "180");
+}
+
+// POSIBLE USO PARA E2//
+
+// function fondo3(){
+//     let SVG = document.getElementById("fondo3");
+//     SVG.setAttribute("width", "500");
+//     SVG.setAttribute("height", "800");
+// }
+
+//Creación de los SVG de imagenes//
+
+function foto1(){
+    let SVG = document.getElementById("foto1");
+    SVG.setAttribute("width", "370");
+    SVG.setAttribute("height", "250");
+}
+
+function foto2(){
+    let SVG = document.getElementById("foto2");
+    SVG.setAttribute("width", "370");
+    SVG.setAttribute("height", "250");
+}
+
+function foto3(){
+    let SVG = document.getElementById("foto3");
+    SVG.setAttribute("width", "370");
+    SVG.setAttribute("height", "250");
+}
+
+function foto4(){
+    let SVG = document.getElementById("foto4");
+    SVG.setAttribute("width", "370");
+    SVG.setAttribute("height", "250");
+}
+
+//Creación de los gráficos//
+
+//Gráfico N°1//
 
 async function fetchData1() {
     const response = await fetch('./namek.csv');
@@ -40,7 +94,7 @@ async function fetchData1() {
     const power = [];
     
     rows.forEach(row => {
-        const cols = row.split(','); // ojo algunos csv tiene separacion ';'
+        const cols = row.split(',');
         char.push((cols[1]));
         power.push(parseFloat(cols[2]));
     });
@@ -70,12 +124,18 @@ function graf1() {
         
         const layout = {
             title: 'Saga de Namek',
-            width: '745'
+            width: '550',
+            height: '250',
+            yaxis: {
+                title: "Cantidad de ki"
+            }
         };
 
         Plotly.newPlot('barchart', [trace], layout);
     });
 }
+
+//Gráfico N°2//
 
 async function fetchData2() {
     const response = await fetch('./force.csv');
@@ -88,7 +148,7 @@ async function fetchData2() {
     const power = [];
     
     rows.forEach(row => {
-        const cols = row.split(','); // ojo algunos csv tiene separacion ';'
+        const cols = row.split(',');
         char.push((cols[1]));
         power.push(parseFloat(cols[2]));
     });
@@ -101,7 +161,7 @@ function graf2() {
         let nuevoDiv = document.createElement('div');
 
         // Asignar un id único al nuevo div
-        nuevoDiv.id = 'barchart 1';
+        nuevoDiv.id = 'barchart1';
 
         // Insertar el nuevo div dentro de un contenedor existente
         document.body.appendChild(nuevoDiv);
@@ -110,16 +170,26 @@ function graf2() {
             x: data.char,
             y: data.power,
             type: 'bar',
+            marker: {
+                color: ['rgba(255, 153, 106, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)',
+                    'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)']
+            }
         };
         
         const layout = {
             title: 'Saga de las Fuerzas Especiales Ginyu',
-            width: '745'
+            width: '550',
+            height: '250',
+            yaxis: {
+                title: "Cantidad de ki"
+            }
         };
 
-        Plotly.newPlot('barchart 1', [trace], layout);
+        Plotly.newPlot('barchart1', [trace], layout);
     });
 }
+
+//Gráfico N°3//
 
 async function fetchData3() {
     const response = await fetch('./ginyu.csv');
@@ -132,7 +202,7 @@ async function fetchData3() {
     const power = [];
     
     rows.forEach(row => {
-        const cols = row.split(','); // ojo algunos csv tiene separacion ';'
+        const cols = row.split(',');
         char.push((cols[1]));
         power.push(parseFloat(cols[2]));
     });
@@ -145,7 +215,7 @@ function graf3() {
         let nuevoDiv = document.createElement('div');
 
         // Asignar un id único al nuevo div
-        nuevoDiv.id = 'barchart 2';
+        nuevoDiv.id = 'barchart2';
 
         // Insertar el nuevo div dentro de un contenedor existente
         document.body.appendChild(nuevoDiv);
@@ -154,16 +224,26 @@ function graf3() {
             x: data.char,
             y: data.power,
             type: 'bar',
+            marker: {
+                color: ['rgba(135, 145, 255, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)',
+                    'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)']
+            }
         };
         
         const layout = {
             title: 'Saga del Capitán Ginyu',
-            width: '745'
+            width: '550',
+            height: '250',
+            yaxis: {
+                title: "Cantidad de ki"
+            }
         };
 
-        Plotly.newPlot('barchart 2', [trace], layout);
+        Plotly.newPlot('barchart2', [trace], layout);
     });
 }
+
+//Gráfico N°4//
 
 async function fetchData4() {
     const response = await fetch('./freezer.csv');
@@ -176,7 +256,7 @@ async function fetchData4() {
     const power = [];
     
     rows.forEach(row => {
-        const cols = row.split(','); // ojo algunos csv tiene separacion ';'
+        const cols = row.split(',');
         char.push((cols[1]));
         power.push(parseFloat(cols[2]));
     });
@@ -189,7 +269,7 @@ function graf4() {
         let nuevoDiv = document.createElement('div');
 
         // Asignar un id único al nuevo div
-        nuevoDiv.id = 'barchart 3';
+        nuevoDiv.id = 'barchart3';
 
         // Insertar el nuevo div dentro de un contenedor existente
         document.body.appendChild(nuevoDiv);
@@ -198,23 +278,44 @@ function graf4() {
             x: data.char,
             y: data.power,
             type: 'bar',
+            marker: {
+                color: ['rgba(255, 255, 148, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)',
+                    'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)', 'rgba(182, 182, 182, 0.8)']
+            }
         };
         
         const layout = {
             title: 'Saga de Freezer',
-            width: '745'
+            width: '550',
+            height: '250',
+            yaxis: {
+                title: "Cantidad de ki"
+            }
         };
 
-        Plotly.newPlot('barchart 3', [trace], layout);
+        Plotly.newPlot('barchart3', [trace], layout);
     });
 }
 
+
+//Función de activación//
 function activartodo(){
     namek();
     graf1();
+    graf2();
+    graf3();
+    graf4();
     fondo1();
     fondo2();
-    fondo3();
+    enlace1();
+    enlace2();
+    enlace3();
+    enlace4();
+    foto1();
+    foto2();
+    foto3();
+    foto4();
 }
 
+//Activación de todas las funciones con la función "activartodo()//
 activartodo()
