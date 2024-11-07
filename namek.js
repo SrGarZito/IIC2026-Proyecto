e5d07a452175908f13ca4132cfe4482a5466ac63
GABRIEL CRUZ <cruzgong@gmail.com>
2024-11-07 10:55:34 -0300

    modified:   index.html 	modified:   namek.js     new file:   owned.gif 	modified:   style.css

--- namek.js
+++ namek.js
@@ -38,244 +38,99 @@ async function fetchData1() {
     return { char, power, saga };
 };
 
+async function fetchData1() {
+    const response = await fetch('./df.csv');
+    const data = await response.text();
+    
+    // Split data into rows
+    const rows = data.split('\n').slice(1);
+    
+    const char = [];
+    const power = [];
+    const saga = [];
+    
+    rows.forEach(row => {
+        const cols = row.split(',');
+        char.push(cols[1]);
+        power.push(parseFloat(cols[2]));
+        saga.push(cols[3]);
+    });
+
+    return { char, power, saga };
+};
+
 function graf1() {
     fetchData1().then(data => {
         let nuevoDiv = document.createElement('div');
+        var myPlot = document.getElementById('scatter');
 
         nuevoDiv.id = 'scatter';
-
         document.body.appendChild(nuevoDiv);
 
         var filteredData = data.char.map((c, index) => {
             return c === 'Freezer' ? { char: c, power: data.power[index], saga: data.saga[index] } : null;
-        }).filter(item => item)
+        }).filter(item => item);
 
         const trace = {
             x: filteredData.map(item => item.saga),
             y: filteredData.map(item => item.power),
             type: 'scatter',
-            marker: {
-                color: ['black'],
-            },
-            line: {
-                color: 'purple',
-                width:0.7
-            },
+            marker: { color: ['black'] },
+            line: { color: 'purple', width: 0.7 },
             width: 0.5,
             showlegend: false,
             name: 'Freezer'
         };
 
-        var filteredData = data.char.map((c, index) => {
+        var filteredData1 = data.char.map((c, index) => {
             return c === 'Goku' ? { char: c, power: data.power[index], saga: data.saga[index] } : null;
-        }).filter(item => item)
+        }).filter(item => item);
 
         const trace1 = {
-            x: filteredData.map(item => item.saga),
-            y: filteredData.map(item => item.power),
+            x: filteredData1.map(item => item.saga),
+            y: filteredData1.map(item => item.power),
             type: 'scatter',
-            marker: {
-                color: ['black']
-            },
-            line: {
-                color: 'orange',
-                width:0.7
-            },
+            marker: { color: ['black'] },
+            line: { color: 'orange', width: 0.7 },
             width: 0.5,
             showlegend: false,
             name: 'Goku'
         };
 
-        var filteredData = data.char.map((c, index) => {
+        var filteredData2 = data.char.map((c, index) => {
             return c === 'Vegeta' ? { char: c, power: data.power[index], saga: data.saga[index] } : null;
-        }).filter(item => item)
+        }).filter(item => item);
 
         const trace2 = {
-            x: filteredData.map(item => item.saga),
-            y: filteredData.map(item => item.power),
+            x: filteredData2.map(item => item.saga),
+            y: filteredData2.map(item => item.power),
             type: 'scatter',
-            marker: {
-                color: ['black']
-            },
-            line: {
-                color: 'blue',
-                width:0.7
-            },
+            marker: { color: ['black'] },
+            line: { color: 'blue', width: 0.7 },
             width: 0.5,
             showlegend: false,
             name: 'Vegeta'
         };
 
-        var filteredData = data.char.map((c, index) => {
+        var filteredData3 = data.char.map((c, index) => {
             return c === 'Krillin' ? { char: c, power: data.power[index], saga: data.saga[index] } : null;
-        }).filter(item => item)
+        }).filter(item => item);
 
         const trace4 = {
-            x: filteredData.map(item => item.saga),
-            y: filteredData.map(item => item.power),
+            x: filteredData3.map(item => item.saga),
+            y: filteredData3.map(item => item.power),
             type: 'scatter',
-            marker: {
-                color: ['black']
-            },
-            line: {
-                color: 'red',
-                width:0.7
-            },
+            marker: { color: ['black'] },
+            line: { color: 'red', width: 0.7 },
             width: 0.5,
             showlegend: false,
             name: 'Krillin'
         };
 
-        const tracefantasma = {
-            x: data.saga,
-            y: data.power,
-            name: 'Freezer',
-            type: 'scatter',
-            marker: {
-                color: 'purple'
-            },
-            visible: 'legendonly'
-        };
-
-        const tracefantasma2 = {
-            x: data.saga,
-            y: data.power,
-            name: 'Goku',
-            type: 'scatter',
-            marker: {
-                color: 'orange'
-            },
-            visible: 'legendonly'
-        };
-
-        const tracefantasma3 = {
-            x: data.saga,
-            y: data.power,
-            name: 'Vegeta',
-            type: 'scatter',
-            marker: {
-                color: 'blue'
-            },
-            visible: 'legendonly'
-        };
-
-        const tracefantasma4 = {
-            x: data.saga,
-            y: data.power,
-            name: 'Krillin',
-            type: 'scatter',
-            marker: {
-                color: 'red'
-            },
-            visible: 'legendonly'
-        };
-
-        const f1 = {
-            x: ['Saga de Namek'],
-            y: [220000],
-            mode: 'markers+text',
-            type: 'scatter',
-            marker: {
-                color: 'purple',
-                size: 10 
-            },
-            text: ['<b>Freezer</b><br>Calmado'], 
-            textposition: 'top left',
-            showlegend: false,
-        };
-
-        const f2 = {
-            x: ['Saga del Capitán Ginyu'],
-            y: [530000],
-            mode: 'markers+text',
-            type: 'scatter',
-            marker: {
-                color: 'red',
-                size: 10
-            },
-            text: ['<b>Freezer</b><br>Poder Base'], 
-            textposition: 'top left',
-            showlegend: false,
-        };
-
-        const g1 = {
-            x: ['Saga de las Fuerzas Especiales Ginyu'],
-            y: [90000],
-            mode: 'markers+text',
-            type: 'scatter',
-            marker: {
-                color: 'blue',
-                size: 10
-            },
-            text: ['<b>Goku</b><br>Aterriza en Namek'],
-            textposition: 'top left',
-            showlegend: false,
-        };
-
-        const g2 = {
-            x: ['Saga del Capitán Ginyu'],
-            y: [180000],
-            mode: 'markers+text',
-            type: 'scatter',
-            marker: {
-                color: 'blue', // Color del punto
-                size: 10 // Tamaño del punto
-            },
-            text: ['<b>Goku</b><br>Enfrentamiento con<br>el capitán Ginyu'], // Texto en formato HTML
-            textposition: 'bottom center', // Posición del texto
-            showlegend: false,
-        };
-
-        const g3 = {
-            x: ['Saga de Freezer'],
-            y: [180000000],
-            mode: 'markers+text',
-            type: 'scatter',
-            marker: {
-                color: 'blue', 
-                size: 10 
-            },
-            text: ['<b>Goku</b><br>Se transforma en SSJ'],
-            textposition: 'top left', 
-            showlegend: false,
-        };
-
-        const k1 = {
-            x: ['Saga de Freezer'],
-            y: [75000],
-            mode: 'markers+text',
-            type: 'scatter',
-            marker: {
-                color: 'black',
-                size: 10 
-            },
-            text: ['<b>Krillin (Muerto)</b><br>Asesinado por Freezer'], 
-            textposition: 'top left', 
-            showlegend: false,
-        };
-
-        const v1 = {
-            x: ['Saga de Freezer'],
-            y: [3000000],
-            mode: 'markers+text',
-            type: 'scatter',
-            marker: {
-                color: 'black', // Color del punto
-                size: 10 // Tamaño del punto
-            },
-            text: ['<b>Vegeta (Muerto)</b><br>Asesinado por Freezer'], // Texto en formato HTML
-            textposition: 'top left', // Posición del texto
-            showlegend: false,
-        };
-
-
         const layout = {
             title: {
                 text: 'Cronología de Namek con los niveles de poder',
-                font: {
-                    size: 18,
-                    color: 'black'
-                }
+                font: { size: 18, color: 'black' }
             },
             width: 1500,
             height: 750,
@@ -286,26 +141,50 @@ function graf1() {
                 autorange: true
             },
             legend: {
-                font: {
-                    size: 14,
-                    color: 'black',
-                },
+                font: { size: 14, color: 'black' },
                 x: -600,
-                y:1.1,
+                y: 1.1,
             },
         };
 
-        Plotly.newPlot('scatter', [trace, f1, f2, trace1, g1, g2, g3, trace2, k1, trace4, v1,
-            tracefantasma, tracefantasma2, tracefantasma3, tracefantasma4], layout);
+        data = [trace, trace1, trace2, trace4];
+        Plotly.newPlot('scatter', data, layout);
+
+        // Create a div to display the image
+        var imageDiv = document.getElementById('hover-image');
+
+        // Image paths for each character
+        const characterImages = {
+            'Freezer': 'n1.jpg',
+            'Goku': 'n4.jpg',
+            'Vegeta': 'n4.jpg',
+            'Krillin': 'esf2.png'
+        };
+
+        // Click event to display image
+        myPlot.on('plotly_click', function(eventData) {
+            const point = eventData.points[0];
+            const character = point.data.name; // Get character name (Freezer, Goku, etc.)
+
+            // Check if the image exists for the character
+            if (characterImages[character]) {
+                imageDiv.style.display = 'block'; // Show the image
+                imageDiv.style.left = `${eventData.event.clientX + 10}px`; // Position near mouse
+                imageDiv.style.top = `${eventData.event.clientY + 10}px`;
+                imageDiv.innerHTML = `<img src="${characterImages[character]}" alt="${character}" style="max-width: 100px; max-height: 100px;"/>`;
+            }
+        });
     });
 }
 
 
 //Función de activación//
 function activartodo(){
+    
     graf1();
     fondo1();
 }
+
 
 //Activación de todas las funciones con la función "activartodo()//
 activartodo()
