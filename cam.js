ad74e88df8cb6bec8e9d1f780294e76718dbb31e
GABRIEL CRUZ <cruzgong@gmail.com>
2024-12-03 12:28:25 -0300

    aaaa

--- cam.js
+++ cam.js
@@ -2,7 +2,7 @@ import Protobject from './js/protobject.js';
 import Aruco from './js/aruco.js';
 
 // Inicializa el sistema de detección de marcadores Aruco
-Aruco.start(30, 1); // La cámara genera eventos cada 30 ms y utiliza la cámara con id 1
+Aruco.start(30, 0); // La cámara genera eventos cada 30 ms y utiliza la cámara con id 1
 Aruco.showPreview({ top: 0, left: 0, width: 1280 / 3, height: 720 / 3 }); // Muestra la vista previa de la cámara
 
 // Escucha los datos de los marcadores Aruco
@@ -10,7 +10,7 @@ Aruco.onData((data) => {
   //console.log(data); // Muestra los datos en la consola para depuración
 
   // Identificadores de marcadores que se desea verificar
-  const markerIds = [200, 1, 100]; 
+  const markerIds = [200, 1, 100, 0]; 
   let markers = []; // Array para almacenar los marcadores detectados
 
   // Recorre los identificadores de marcadores y verifica su presencia en los datos
@@ -20,7 +20,6 @@ Aruco.onData((data) => {
       markers.push({ id: id, y: data[id].position.y });
     }
   }
-
   // Ordena los marcadores detectados en función de su posición en el eje Y (de arriba hacia abajo)
   markers.sort((a, b) => b.y - a.y);
 
@@ -28,5 +27,6 @@ Aruco.onData((data) => {
   let orderPosition = markers.map(marker => marker.id);
 
   // Envía los IDs ordenados al sistema principal
-  Protobject.send(orderPosition).to('main.js');
+  console.log(orderPosition)
+  Protobject.send(orderPosition).to('namek.js');
 });
